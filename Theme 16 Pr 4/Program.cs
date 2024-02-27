cd76d44bbb1be29cd07069af37ca127dd6bbe3e0
egorduba1301 <126455031+egorduba1301@users.noreply.github.com>
2024-02-27 05:50:31 +0200

    Theme 16 Pr 4
    Theme 16 Pr 4

---
+++ Theme 16 Pr 4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme_16_Pr_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Staff[] staffs = new Staff[1];
+            for (int i = 0; i < staffs.Length; i++)
+            {
+                int nomber = int.Parse(Console.ReadLine());
+                string name = Console.ReadLine();
+                string surname = Console.ReadLine();
+                string ochestvo = Console.ReadLine();
+                DateTime data = DateTime.Parse(Console.ReadLine());
+                string doljnost = Console.ReadLine();
+                double oclad = double.Parse(Console.ReadLine());
+
+                staffs[i] = new Staff(nomber, name, surname, ochestvo, data, doljnost, oclad);
+            }
+            foreach (var staff in staffs)
+
+            {
+                Console.WriteLine(staff.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
---
+++ Theme 16 Pr 4/Staff.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme_16_Pr_4
+{
+    internal class Staff
+    {
+        /// <summary>
+        /// поле опсывающее номре
+        /// </summary>
+        int nomber;
+        /// <summary>
+        /// поле описывающее имя
+        /// </summary>
+        string name;
+        /// <summary>
+        /// поле описывающее фамилию
+        /// </summary>
+        string surname;
+        /// <summary>
+        /// поле описывающее отчество
+        /// </summary>
+        string othestvo;
+        /// <summary>
+        /// поле описывающее дату 
+        /// </summary>
+        DateTime data;
+        /// <summary>
+        /// поле описывающее должность
+        /// </summary>
+        string doljnost;
+        /// <summary>
+        /// поле поисывающее оклад
+        /// </summary>
+        double oclad;
+        /// <summary>
+        /// свойство описывающее поле номер
+        /// </summary>
+        public int Nomber
+        {
+            get
+            {
+                return nomber;
+            }
+            set
+            {
+                nomber = value;
+            }
+        }
+        /// <summary>
+        /// свойство описывающее поле имя 
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return (name == "") ? "имя не введено" : name;
+            }
+            set
+            {
+                name = value; ;
+            }
+        }
+        /// <summary>
+        /// свойство описывающее поле фаимилия 
+        /// </summary>
+        public string Surname
+        {
+            get
+            {
+                return (surname == "") ? "фамилия  не введена" : surname;
+            }
+            set
+            {
+                surname = value; ;
+            }
+        }
+        /// <summary>
+        /// свойство описывающее поле отчество
+        /// </summary>
+        public string Othestvo
+        {
+            get
+            {
+                return (othestvo == "") ? "фамилия  не введена" : othestvo;
+            }
+            set
+            {
+                othestvo = value; ;
+            }
+        }
+        /// <summary>
+        /// свойство описывающее поле дата
+        /// </summary>
+        public DateTime Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value; ;
+            }
+        }
+        /// <summary>
+        /// свойство описывающее поле должность
+        /// </summary>
+
+        public string Doljnost
+        {
+            get
+            {
+                return (doljnost == "") ? "фамилия  не введена" : doljnost;
+            }
+            set
+            {
+                doljnost = value; ;
+            }
+        }
+        /// <summary>
+        /// свойство описывающее поле оклад
+        /// </summary>
+        public double Oclad
+        {
+            get
+            {
+                return oclad;
+            }
+            set
+            {
+                oclad = value; ;
+            }
+        }
+        /// <summary>
+        /// конструктор без параметров 
+        /// </summary>
+        public Staff() { }
+        /// <summary>
+        /// конструктор с параметрам 
+        /// </summary>
+        /// <param name="nomber"></param>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="othestvo"></param>
+        /// <param name="data"></param>
+        /// <param name="doljnost"></param>
+        /// <param name="oclad"></param>
+        public Staff(int nomber, string name, string surname, string othestvo, DateTime data, string doljnost, double oclad)
+        {
+            this.Nomber = nomber;
+            this.Name = name;
+            this.Surname = surname;
+            this.Othestvo = othestvo;
+            this.Data = data;
+            this.Doljnost = doljnost;
+            this.Oclad = oclad;
+
+        }
+        /// <summary>
+        /// метод для выводв информации 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $" номер:{nomber} имя:{name} фамилия:{surname} отчество:{othestvo} дата:{data} должность:{doljnost}  оклад:{oclad}\n ";
+        }
+    }
+
+}
+
