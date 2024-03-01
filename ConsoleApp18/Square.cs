b2eaac332ed6d3a520d632df98f208f6449606a5
egorduba1301 <126455031+egorduba1301@users.noreply.github.com>
2024-03-01 12:07:53 +0200

    Theme 18 Pr 3
    Theme 18 Pr 3

---
+++ ConsoleApp18/Square.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Square : Rectangle
+    {
+
+
+        public Square(double side) : base("Square", side, side)
+        {
+        }
+    }
+
+    class Triangle : Figure
+    {
+        private double _base;
+        private double _height;
+
+        public Triangle(string name, double @base, double height) : base(name)
+        {
+            _base = @base;
+            _height = height;
+        }
+
+        public override double Area()
+        {
+            return 0.5 * _base * _height;
+        }
+
+        public override double Perimeter()
+        {
+
+            return 3 * _base;
+        }
+    }
+
+    class Circle : Figure
+    {
+        private double _radius;
+
+        public Circle(string name, double radius) : base(name)
+        {
+            _radius = radius;
+        }
+
+        public override double Area()
+        {
+            return 3.14 * _radius * _radius;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * 3.14 * _radius;
+        }
+    }
+}
