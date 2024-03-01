3953384272e5d8f0ae26bd203c223e8eb47cc992
egorduba1301 <126455031+egorduba1301@users.noreply.github.com>
2024-03-01 12:09:27 +0200

    Theme 18 Pr 3
    Theme 18 Pr 3

---
+++ Theme 18 Pr 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            {
+                Figure[] figures = new Figure[]
+                {
+            new Rectangle("Rectangle", 5, 3),
+            new Square(4),
+            new Triangle("Triangle", 6, 4),
+            new Circle("Circle", 2)
+                };
+
+                foreach (var figure in figures)
+                {
+                    figure.Print();
+                    Console.WriteLine($"Area: {figure.Area()}, Perimeter: {figure.Perimeter()}");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Theme 18 Pr 3/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Rectangle : Figure
+    {
+
+
+        private double _side1;
+        private double _side2;
+
+        public Rectangle(string name, double side1, double side2) : base(name)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+
+        public double Side1 => _side1;
+        public double Side2 => _side2;
+
+        public override double Area()
+        {
+            return _side1 * _side2;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * (_side1 + _side2);
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Side 1: {_side1}, Side 2: {_side2}");
+        }
+    }
+
+}
