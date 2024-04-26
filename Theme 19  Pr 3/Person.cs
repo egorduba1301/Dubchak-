87d62be496017449e7184c3655b9ccda957735b6
egorduba1301 <126455031+egorduba1301@users.noreply.github.com>
2024-04-26 21:17:14 +0300

    Theme 19 Pr 3
    Theme 19 Pr 3

---
+++ Theme 19  Pr 3/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme_19__Pr_3
+{
+    internal class Person : ICompareTo
+    {
+        string Name { get; set; }
+        public int Age { get; set; }
+        public Person() { }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public int CompareTo(Person obj)
+        {
+            return Name.CompareTo(obj.Name);
+        }
+        public override string ToString()
+        {
+            return $" Имя:{Name}\tВозраст:{Age}";
+        }
+    }
+}
